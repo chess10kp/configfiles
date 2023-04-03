69844cedd1afe422f1d2a96c5131cc73c5e82741
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2023-04-03 11:03:43 -0400

    nvchad

--- nvim/lua/plugins/configs/mason.lua
+++ nvim/lua/plugins/configs/mason.lua
@@ -1,6 +1,28 @@
-return function(_, opts)
-  require("mason").setup(opts)
-  for _, plugin in ipairs { "mason-lspconfig", "mason-null-ls", "mason-nvim-dap" } do
-    pcall(require, plugin)
-  end
-end
+local options = {
+  ensure_installed = { "lua-language-server" }, -- not an option from mason.nvim
+
+  PATH = "skip",
+
+  ui = {
+    icons = {
+      package_pending = " ",
+      package_installed = " ",
+      package_uninstalled = " ﮊ",
+    },
+
+    keymaps = {
+      toggle_server_expand = "<CR>",
+      install_server = "i",
+      update_server = "u",
+      check_server_version = "c",
+      update_all_servers = "U",
+      check_outdated_servers = "C",
+      uninstall_server = "X",
+      cancel_installation = "<C-c>",
+    },
+  },
+
+  max_concurrent_installers = 10,
+}
+
+return options
