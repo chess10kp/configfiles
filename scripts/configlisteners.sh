b0c3925ac49b831aa4c99363de094ff0211bf8c6
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2023-10-08 01:42:24 -0400

    fix:small updates

--- scripts/configlisteners.sh
+++ scripts/configlisteners.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-config_files = ( "~/.config/waybar/config" )
-for file in "{$config_files[@]}"; do 
-  echo "$file"
+config_files=( '~/.config/waybar/config' '~/.config/waybar/style.css' )
+for file in "${config_files[@]}"; do
+    echo "$file" | entr -c 
 done
