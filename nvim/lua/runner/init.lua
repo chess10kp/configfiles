86795ef60bb8628200f6513bcf863aacdf3d0a1a
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-06-28 00:50:54 -0400

    feat: new plugin

--- nvim/lua/runner/init.lua
+++
@@ -1,8 +0,0 @@
-local runner = {}
-print("hi")
-
-function runner.run_job()
-  print("Running job...")
-end
-
-return runner 
--- nvim/lua/runner/lua/runner.lua
+++ nvim/lua/runner/lua/runner.lua
@@ -1,20 +1,109 @@
 local M = {}
 
-print("loaded runner.lua")
-
-local function generate_keymaps(filetype, command, keymap)
-  vim.api.nvim_buf_set_keymap("n", "<leader>rf", command)
+-- @param cmd String default cmd
+-- @param bufnr Int
+local function get_cmd(cmd, bufnr)
+	if vim.b.runner_cmd == nil then
+		local run_cmd = vim.fn.input({ prompt = "Compile command: ", default = cmd .. " ", cancelreturn = "" })
+		if run_cmd == "" then
+			return nil
+		end
+		vim.api.nvim_buf_set_var(bufnr, "runner_cmd", run_cmd)
+	end
+	return vim.api.nvim_buf_get_var(bufnr, "runner_cmd")
 end
 
-M.setup = function(opts)
-	print("Setup has been called ")
+-- 
+-- @param bufnr Int
+--
+local function set_cmd(bufnr)
+	local cmd = vim.b.runner_cmd
+	local run_cmd = vim.fn.input({ prompt = "Compile command: ", default = cmd .. " ", cancelreturn = "" })
+	if run_cmd == "" then
+		return nil
+	end
+	vim.api.nvim_buf_set_var(bufnr, "runner_cmd", run_cmd)
+	return vim.api.nvim_buf_get_var(bufnr, "runner_cmd")
+end
 
-	filetypes = {
-		python = {},
-		javascript = {},
-		cpp = {},
-		c = {},
+
+-- 
+-- @param cmd String command to run in terminal 
+--
+local function run_in_terminal(cmd)
+    local rows  = vim.api.nvim_get_option("lines")
+    local split_rows = rows * 0.3
+    vim.cmd( split_rows .. "split")
+	local buf = vim.api.nvim_create_buf(false, true)
+    local win = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_buf(win, buf)
+    vim.fn.termopen(cmd)
+
+	vim.api.nvim_win_set_option(win, "number", false)
+	vim.api.nvim_win_set_option(win, "relativenumber", false)
+
+    vim.api.nvim_buf_set_keymap(buf, "n",  "q", ":bwipeout!<CR>", { noremap = true, silent = true})
+
+	-- local width = vim.api.nvim_get_option("columns")
+	-- local height = vim.api.nvim_get_option("lines")
+	-- local win_width = 50
+	-- local win_height = 10
+	-- local win_opts = {
+	-- 	relative = "editor",
+	-- 	width = win_width,
+	-- 	height = win_height,
+	-- 	row = math.ceil((height - win_height) / 2),
+	-- 	col = math.ceil((width - win_width) / 2),
+	-- }
+end
+
+
+-- @param file String name of ft
+-- @param cmd String default command
+local function make_maps(ft, c)
+	vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+		pattern = "*." .. ft,
+		group = "Runner",
+		callback = function(ev)
+			local bufnr = ev.buf
+			vim.api.nvim_buf_create_user_command(bufnr, "RunnerRun", function()
+				set_cmd(bufnr)
+			end, {})
+			vim.keymap.set("n", "<leader>cc", function()
+				local cmd = get_cmd(c, bufnr)
+				if cmd == nil then
+					return
+				end
+                run_in_terminal(cmd)
+			end, { buffer = true })
+		end,
+	})
+end
+
+-- @param opts Table
+-- @param opts.filetypes Table
+-- @param opts.cmd String
+-- @param opts.key String
+--
+M.setup = function(opts)
+	vim.b.runner_cmd = nil
+	local filetypes = {
+		lua = { "lua" },
+		py = { "python", "-u" },
+		js = { "node" },
+		ts = { "node" },
+		cpp = { "make" },
+		c = { "make" },
+		hs = { "ghc" },
+		go = { "go" },
 	}
+	vim.api.nvim_create_augroup("Runner", {
+		clear = true,
+	})
+	-- nvim_create_autocmd is broken
+	for file, cmd in pairs(filetypes) do
+		make_maps(file, table.concat(cmd, " "))
+	end
 
 end
 
