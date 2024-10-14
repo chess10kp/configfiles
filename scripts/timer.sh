67f3479ee766fb9794252ffde34357ca1f4576c8
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-10-14 13:20:21 -0400

    feat(timer): include hours in timer.sh

--- scripts/timer.sh
+++ scripts/timer.sh
@@ -2,11 +2,13 @@
 # shellcheck disable=1090,2034,2207,2154,2086,2216
 
 source ~/.config/scripts/configvars.sh
-alarm_time=$(printf "" | $dmenu )
+alarm_time=$(printf "" | $dmenu_prompt "Set timer: " )
 
 [[ $alarm_time == "" ]] && exit 0
 
 case "$(echo $alarm_time | sed 's/[0-9]*//g')" in 
+    h)
+      notify-send -a "Timer" "set for $alarm_time"
     m)
         notify-send -a "Timer" "set for $alarm_time"
         ;;
@@ -21,4 +23,4 @@ esac
 echo "timer started at $(date '+%H%M')"
 timeout $alarm_time cat - 
 mpv --no-video /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga & 
-notify-send -a "Timer" -t 3000 "timer complete"
+notify-send -a "Timer" -t 3000 "timer compleTe"
