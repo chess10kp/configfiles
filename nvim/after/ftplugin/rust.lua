efc2f9f3c29679c015a00dc91aa8c28d61f434e3
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2025-02-13 12:22:55 -0500

    add blink and rustacean

---
+++ nvim/after/ftplugin/rust.lua
@@ -0,0 +1,9 @@
+-- rustacean
+local bufnr = vim.api.nvim_get_current_buf()
+vim.keymap.set("n", "ga", function()
+	vim.cmd.RustLsp("codeAction")
+end, { silent = true, buffer = bufnr })
+
+vim.keymap.set("n", "K", function()
+	vim.cmd.RustLsp({ "hover", "actions" })
+end, { silent = true, buffer = bufnr })
